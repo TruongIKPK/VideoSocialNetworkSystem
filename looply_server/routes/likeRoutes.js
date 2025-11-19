a4684048efdfb99269e44a68fc202aebd5e09c60
Lê Nguyễn Phi Trường <phitruong20192020@gmail.com>
2025-11-19 11:36:30 +0700

    update checklike

--- looply_server/routes/likeRoutes.js
+++ looply_server/routes/likeRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { like, unlike } from "../controllers/likeController.js";
+import { like, unlike, checkLike } from "../controllers/likeController.js";
 
 const router = express.Router();
 
 router.post("/like", like);     // like nội dung
 router.post("/unlike", unlike); // bỏ like
+router.get("/check", checkLike); // kiểm tra đã like chưa
 
 export default router;
